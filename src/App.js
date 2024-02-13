ed1e855c088df18fe338c5c8a9c5544b2ca0da2c
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-02-13 12:24:56 +1100

    set the route of components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import {Switch,Route,Link} from "react-router-dom"
+import {Routes,Route,Link} from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import { BrowserRouter } from 'react-router-dom'
 
 import AddReview from './components/add-review';
 import MoviesList from './components/movies-list';
@@ -10,19 +11,38 @@ import Movie from './components/movie';
 import Login from './components/login';
 
 function App() {
+  const [user,setUser] = React.useState(null)
+
+  async function login (user=null){
+    setUser(user)
+  }
+  async function logout(){
+    setUser(null)
+  }
+
   return (
-    <div className='App'>
+    <BrowserRouter className='App'>
       <Navbar bg="light" expand="sm">
-        <Navbar.Brand href = "#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href = "#home">Movie Reviews</Navbar.Brand>
         <Navbar.Toggle aria-controls = "basic-navbar-nav"/>
         <Navbar.Collapse id = "basic-navbar-nav">
           <Nav className='mr-auto'>
-            <Nav.Link href = "#home">Home</Nav.Link>
-            <Nav.Link href = "#link">Link</Nav.Link>
+            <Nav.Link>
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+               {user?(<a onClick={logout}>Logout User</a>):(<Link to={"/login"}>Login</Link>)}
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-    </div>
+      <Routes>
+        <Route path="/movies" element={<MoviesList/>}></Route>
+        <Route path="/movies/:id/review" element={<AddReview user={user}/>}></Route>
+        <Route path="/movies/:id/" element={<Movie user={user}/>}></Route>
+        <Route path="/login" element={<Login login={login}/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
