1396799573cc57308e6259e15221dd4cb908ec9b
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-02-14 14:29:30 +1100

    retrieve data from backend

--- src/components/movies-list.js
+++ src/components/movies-list.js
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { useState ,useEffect} from "react";
+import MovieDataService from "../services/movies.js"
+import { Link } from "react-router-dom";
 
-const MoviesList = ()=>{
+const MoviesList = props=>{
+    const [movies,setMovies]=useState([])
+    const [searchTitle, setSearchTitle]=useState([])
+    const [searchRating,setSearchRating]=useState("")
+    const [ratings,setRatings]=useState(["All Ratings"])
+
+    useEffect(()=>{
+        retrieveMovies()
+        retrieveRatings()
+    },[])
+
+    const retrieveMovies = ()=>{
+        MovieDataService.getAll()
+        .then(response=>{
+            console.log(response.data)
+            setMovies(response.data.movies)
+        })
+        .catch(e=>{
+            console.log(e)
+        })
+    }
+
+    const retrieveRatings = ()=>{
+        MovieDataService.getRatings()
+        .then(response=>{
+            console.log(response.data)
+            setRatings(["All Ratings"].concat(response.data))
+        })
+        .catch(e=>{
+            console.log(e)
+        })
+    }
+
     return(
         <div className="App">
             Movies List
