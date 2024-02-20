b4a20171bdb4857a2389a029575b0a1017115028
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-02-20 11:08:34 +1100

    divide the MoviesList component

--- src/components/movies-list.js
+++ src/components/movies-list.js
@@ -3,6 +3,7 @@ import MovieDataService from "../services/movies.js"
 import { Link } from "react-router-dom";
 import { Form,Button,Col,Row,Container,Card} from 'react-bootstrap'
 
+
 const MoviesList = props=>{
     const [movies,setMovies]=useState([])
     const [searchTitle, setSearchTitle]=useState([])
@@ -63,47 +64,65 @@ const MoviesList = props=>{
     return(
         <div className="App">
             <Container>
-                <Form>
-                    <Row>
-                        <Col>
-                            <Form.Group>
-                                <Form.Control type="text" placeholder="Search by title" value={searchTitle} onChange={onChangeSearchTitle}/>
-                            </Form.Group>
-                        </Col>
-                        <Col>
-                            <Form.Group>
-                                <Form.Control as="select" onChange={onChangeSearchRating}>
-                                    {ratings.map(rating=>{
-                                        return(<option value={rating}>{rating}</option>)
-                                    })}
-                                </Form.Control>
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                    <Button variant="primary" type="button" onClick={findBy}>
-                                Search
-                    </Button>
-                </Form>
-                <Row>
-                    {movies.map((movie)=>{
-                        return(<Col>
-                                    <Card style={{width:'18rem'}}>
-                                        <Card.Img src={movie.poster+"/100px180"}/>
-                                        <Card.Body>
-                                            <Card.Title>{movie.title}</Card.Title>
-                                            <Card.Text>
-                                                Rating:{movie.rated}
-                                            </Card.Text>
-                                            <Card.Text>{movie.plot}</Card.Text>
-                                            <Link to={"/movies/"+movie._id}>View Reviews</Link>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>)
-                    })}
-                </Row>
+                <SearchForm onSearch={findBy} searchTitle={searchTitle} searchRating={searchRating} onChangeSearchTitle={onChangeSearchTitle} onChangeSearchRating={onChangeSearchRating} ratings={ratings}/>
+                <MovieGrid movies={movies}/>
             </Container>
         </div>
     );
+}
+
+const SearchForm =({onSearch,searchRating,searchTitle,onChangeSearchRating,onChangeSearchTitle,ratings})=>{
+    return(
+        <Form>
+            <Row>
+                <Col>
+                    <Form.Group>
+                        <Form.Control type="text" placeholder="Search by title" value={searchTitle} onChange={onChangeSearchTitle}/>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group>
+                        <Form.Control as="select" value={searchRating} onChange={onChangeSearchRating}>
+                            {ratings.map(rating=>(
+                                <option  value={rating}>{rating}</option>
+                            ))}
+                        </Form.Control>
+                    </Form.Group>
+                </Col>
+            </Row>
+            <Button variant="primary" type="button" onClick={onSearch}>
+                        Search
+            </Button>
+        </Form>
+    )
+}
+
+const MovieCard=({movie})=>{
+    return(
+        <Card style={{width:'18rem'}}>
+            <Card.Img variant = "top" src={movie.poster+"/100px180"}/>
+            <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>
+                    <Card.Text>
+                        Rating:{movie.rated}
+                    </Card.Text>
+                <Card.Text>{movie.plot}</Card.Text>
+                <Link to={`/movies/${movie._id}`}>View Reviews</Link>
+            </Card.Body>
+        </Card>
+    )
+}
+
+const MovieGrid=({movies})=>{
+    return(
+        <Row>
+            {movies.map((movie,index)=>(
+                <Col key={index}>
+                    <MovieCard movie={movie}/>
+                </Col>
+            ))}
+        </Row>
+    )
 }
 
 export default MoviesList;
