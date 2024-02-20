83afa0ad00df9dbbb67ec041e74c0f737ed22de1
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-02-20 13:49:50 +1100

    now we can submit reviews editing

--- src/components/add-review.js
+++ src/components/add-review.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React,{useState} from "react";
+import MovieDataService from "../services/movies";
+import { Link,useParams } from "react-router-dom";
+import {Form,Button} from 'react-bootstrap';
 
-const AddReview=()=>{
+const AddReview=props=>{
+    let editing=false
+    let initialReviewState=""
+
+    const [review,setReview]=useState(initialReviewState)
+    const [submitted,setSubmitted]=useState(false)
+    const {id} = useParams();
+
+    const onChangeReview =e=>{
+        const review=e.target.value
+        setReview(review);
+    }
+
+    const saveReview =()=>{
+        var data = {
+            review:review,
+            name:props.user.name,
+            user_id:props.user.id,
+            movie_id:id
+        }
+        MovieDataService.createReview(data)
+        .then(response=>{
+            setSubmitted(true)
+        })
+        .catch(e=>{console.log(e)})
+    }
+
     return(
-        <div className="App">
-            AddReview;
+        <div>
+            {
+                submitted?(
+                    <div>
+                        <h4>Review submitted successfully</h4>
+                        <Link to={"/movies/"+id}>Back to Movie</Link>
+                    </div>
+                ):(
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>{editing?"Edit":"Create"}</Form.Label>
+                            <Form.Control 
+                                type="text"
+                                required
+                                value={review}
+                                onChange={onChangeReview}/>
+                        </Form.Group>
+                        <Button variant="primary" onClick={saveReview}>Submit</Button>
+                    </Form>
+                )
+            }
         </div>
     )
 }
