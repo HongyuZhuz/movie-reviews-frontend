7f885be0887ac7e3ec269953d9772229fbe92a3c
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-02-22 12:01:42 +1100

    change the http portocal address

--- src/services/movies.js
+++ src/services/movies.js
@@ -3,28 +3,28 @@ import axios from "axios";
 class MovieDataService{
 
     getAll(page=0){
-        return axios.get(`http://localhost:5000/api/v1/movies?page=${page}`)
+        return axios.get(`https://peaceful-mesa-18237-772f6179939a.herokuapp.com/api/v1/movies?page=${page}`)
     }
     get(id){
-        return axios.get(`http://localhost:5000/api/v1/movies/id/${id}`)
+        return axios.get(`https://peaceful-mesa-18237-772f6179939a.herokuapp.com/api/v1/movies/id/${id}`)
     }
     find(searchTitle,rating,page=0){
-        return axios.get(`http://localhost:5000/api/v1/movies?title=${searchTitle}&rated=${rating}&page=${page}`)
+        return axios.get(`https://peaceful-mesa-18237-772f6179939a.herokuapp.com/api/v1/movies?title=${searchTitle}&rated=${rating}&page=${page}`)
     }
     createReview(data){
-        return axios.post("http://localhost:5000/api/v1/movies/review",data)
+        return axios.post("https://peaceful-mesa-18237-772f6179939a.herokuapp.com/api/v1/movies/review",data)
     }
     updateReview(data){
-        return axios.put("http://localhost:5000/api/v1/movies/review",data)
+        return axios.put("https://peaceful-mesa-18237-772f6179939a.herokuapp.com/api/v1/movies/review",data)
     }
     deleteReview(id,userId){
         return axios.delete(
-            "http://localhost:5000/api/v1/movies/review",
+            "https://peaceful-mesa-18237-772f6179939a.herokuapp.com/api/v1/movies/review",
             {data:{review_id:id,user_id:userId}}
         )
     }
     getRatings(){
-        return axios.get("http://localhost:5000/api/v1/movies/ratings")
+        return axios.get("https://peaceful-mesa-18237-772f6179939a.herokuapp.com/api/v1/movies/ratings")
     }
 }
 
